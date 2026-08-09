165795644a80fc7ec06b91f1af92ccb9d94936b4
vaibhav <vaibhavvishwakarma0507@gmail.com>
2026-08-10 04:18:47 +0530

    Update multi_mavros.launch.py

--- robofest/launch/multi_mavros.launch.py
+++ robofest/launch/multi_mavros.launch.py
@@ -3,7 +3,7 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        # Drone 1 MAVROS Node
+        # Drone 1 MAVROS Node (Joey)
         Node(
             package='mavros',
             executable='mavros_node',
@@ -11,12 +11,13 @@ def generate_launch_description():
             output='screen',
             parameters=[
                 {'fcu_url': 'udp://127.0.0.1:14550@14555'},
+                {'gcs_url': 'udp://@127.0.0.1:14550'},
                 {'tgt_system': 1},
                 {'tgt_component': 1}
             ]
         ),
         
-        # Drone 2 MAVROS Node
+        # Drone 2 MAVROS Node (DeeDee)
         Node(
             package='mavros',
             executable='mavros_node',
@@ -24,12 +25,13 @@ def generate_launch_description():
             output='screen',
             parameters=[
                 {'fcu_url': 'udp://127.0.0.1:14560@14565'},
+                {'gcs_url': 'udp://@127.0.0.1:14550'},
                 {'tgt_system': 2},
                 {'tgt_component': 1}
             ]
         ),
         
-        # Drone 3 MAVROS Node
+        # Drone 3 MAVROS Node (Marky)
         Node(
             package='mavros',
             executable='mavros_node',
@@ -37,6 +39,7 @@ def generate_launch_description():
             output='screen',
             parameters=[
                 {'fcu_url': 'udp://127.0.0.1:14570@14575'},
+                {'gcs_url': 'udp://@127.0.0.1:14550'},
                 {'tgt_system': 3},
                 {'tgt_component': 1}
             ]
